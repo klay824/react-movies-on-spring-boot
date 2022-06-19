0bb90ea41c5c542beeb171776775713121f9bb4b
Katy Chadwell <kchadwell0226@gmail.com>
2022-06-19 15:53:52 -0500

    spring initializer for base setup

---
+++ src/main/java/com/movies/reactmoviesonspringboot/ReactMoviesOnSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.movies.reactmoviesonspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactMoviesOnSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactMoviesOnSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movies/reactmoviesonspringboot/ReactMoviesOnSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movies.reactmoviesonspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactMoviesOnSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
